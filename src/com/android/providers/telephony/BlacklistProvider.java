bac5ff8e3b8504a4ce4e850f920d09469f7395fb
aways <aways@c-rom.org>
2014-01-26 22:01:07 +0100

    Revert "Improve delete handling code."
    This reverts commit ce2b3550a12acbaa9b01bbd660aa2c7a85ff55a6.

--- src/com/android/providers/telephony/BlacklistProvider.java
+++ src/com/android/providers/telephony/BlacklistProvider.java
@@ -205,28 +205,19 @@ public class BlacklistProvider extends ContentProvider {
 
         switch (match) {
             case BL_ALL:
+                count = db.delete(BLACKLIST_TABLE, where, whereArgs);
                 break;
             case BL_ID:
-                if (where != null || whereArgs != null) {
-                    throw new UnsupportedOperationException(
-                            "Cannot delete URI " + uri + " with a where clause");
-                }
-                where = Blacklist._ID + " = ?";
-                whereArgs = new String[] { uri.getLastPathSegment() };
-                break;
+                count = db.delete(BLACKLIST_TABLE, Blacklist._ID + " = ?",
+                        new String[] { uri.getLastPathSegment() });
             case BL_NUMBER:
-                if (where != null || whereArgs != null) {
-                    throw new UnsupportedOperationException(
-                            "Cannot delete URI " + uri + " with a where clause");
-                }
-                where = COLUMN_NORMALIZED + " = ?";
-                whereArgs = new String[] { normalizeNumber(uri.getLastPathSegment()) };
+                count = db.delete(BLACKLIST_TABLE, COLUMN_NORMALIZED + " = ?",
+                        new String[] { normalizeNumber(uri.getLastPathSegment()) });
                 break;
             default:
                 throw new UnsupportedOperationException("Cannot delete that URI: " + uri);
         }
 
-        count = db.delete(BLACKLIST_TABLE, where, whereArgs);
         if (DEBUG) Log.d(TAG, "delete result count " + count);
 
         if (count > 0) {
